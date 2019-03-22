eefa4acbc7a208e11246d62f3e301a80f4ec0836
David Figatner <david@yopeyopey.com>
2019-03-22 16:47:15 +0800

    values are now emitted with event select

--- code/falldown.js
+++ code/falldown.js
@@ -561,9 +561,9 @@ class FallDown extends Events
             const changed = this.falldown[index]
             changed.selected = !changed.selected
             this.box.childNodes[index].classList.toggle(this.options.classNames.select)
-            const list = this.showSelection()
+            this.showSelection()
             this.setCursor(index)
-            return { changed, list, active: FallDown.active }
+            return { changed, list: this.value, active: FallDown.active }
         }
         else
         {
@@ -581,7 +581,7 @@ class FallDown extends Events
                 }
             }
             this.showSelection()
-            return { changed: this.box.childNodes[index].innerHTML }
+            return { changed: this.falldown[index].value }
         }
     }
 
